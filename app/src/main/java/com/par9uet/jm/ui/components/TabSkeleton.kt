d51a33d9f1e2ffa762560d68594621f6fcbf4df3
Dedicatus546 <qq1607611087@gmail.com>
2026-01-12 11:21:46 +0800

    refactor: 调整首页骨架屏下 tab 的样式

--- app/src/main/java/com/par9uet/jm/ui/components/TabSkeleton.kt
+++ app/src/main/java/com/par9uet/jm/ui/components/TabSkeleton.kt
@@ -1,9 +1,9 @@
 package com.par9uet.jm.ui.components
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,8 +19,9 @@ fun TabSkeleton(index: Int) {
     Box(
         modifier = Modifier
             .width(currentWidth) // 给一个基础宽度
-            .height(20.dp) // 模拟文字高度
             .clip(RoundedCornerShape(4.dp))
             .shimmer()
-    )
+    ) {
+        Text(text = "\u00A0")
+    }
 }
