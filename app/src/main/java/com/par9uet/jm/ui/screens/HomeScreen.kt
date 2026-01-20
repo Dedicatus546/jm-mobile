8199710895e76b7c792c849887e9d7fd72d68062
Dedicatus546 <qq1607611087@gmail.com>
2026-01-20 09:28:42 +0800

    fix: 修复首页列表在列表项个数较少时没有从顶部排列问题

--- app/src/main/java/com/par9uet/jm/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/par9uet/jm/ui/screens/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -139,6 +140,7 @@ fun HomeScreen(
         ) { page ->
             val comicList = homeComicState.list.getOrNull(page)?.list ?: listOf()
             PullToRefreshBox(
+                modifier = Modifier.fillMaxSize(),
                 isRefreshing = homeComicState.isLoading,
                 onRefresh = {
                     comicViewModel.getHomeComic()
