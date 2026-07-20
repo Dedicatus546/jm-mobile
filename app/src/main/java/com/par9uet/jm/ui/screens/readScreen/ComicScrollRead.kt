f79ed29286c92aa5921631aa017b5aac9d33d2bc
Dedicatus546 <qq1607611087@gmail.com>
2026-07-20 11:11:53 +0800

    feat: 阅读页滚动模式支持缩放（放大）图片，调整了点击屏幕调整上一页、下一页的逻辑，改为滚动一个屏幕的高度，而不是滚动到下一张图片

--- app/src/main/java/com/par9uet/jm/ui/screens/readScreen/ComicScrollRead.kt
+++ app/src/main/java/com/par9uet/jm/ui/screens/readScreen/ComicScrollRead.kt
@@ -1,37 +1,104 @@
 package com.par9uet.jm.ui.screens.readScreen
 
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalViewConfiguration
+import androidx.compose.ui.platform.ViewConfiguration
 import androidx.compose.ui.unit.dp
+import com.par9uet.jm.data.models.ComicPicImageState
 import com.par9uet.jm.data.models.ImageResultState
-import com.par9uet.jm.ui.components.ComicPicImage
 import com.par9uet.jm.ui.viewModel.ComicReadViewModel
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
+import me.saket.telephoto.zoomable.ZoomSpec
+import me.saket.telephoto.zoomable.rememberZoomableState
+import me.saket.telephoto.zoomable.zoomable
 import org.koin.androidx.compose.koinViewModel
 import kotlin.time.Duration.Companion.milliseconds
+
+@Composable
+private fun ComicPicImage(
+    modifier: Modifier = Modifier,
+    comicPicImageState: ComicPicImageState,
+    contentScale: ContentScale = ContentScale.FillBounds,
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
+    val imageResult = comicPicImageState.imageResultState
+
+    val retryImageDecode = {
+        coroutineScope.launch {
+            comicPicImageState.decode(context)
+        }
+    }
+
+    Box(modifier = modifier) {
+        when (imageResult) {
+            is ImageResultState.Loading -> {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+
+            is ImageResultState.Failure -> {
+                Column(
+                    modifier = Modifier.align(Alignment.Center),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(imageResult.reason)
+                    TextButton(
+                        onClick = {
+                            retryImageDecode()
+                        }
+                    ) {
+                        Text("重试")
+                    }
+                }
+            }
+
+            is ImageResultState.Success -> {
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = contentScale,
+                    bitmap = imageResult.decodeImageBitmap,
+                    contentDescription = "第${comicPicImageState.index}张图片",
+                )
+            }
+        }
+    }
+}
 
 @OptIn(FlowPreview::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -74,77 +141,93 @@ fun ComicScrollRead(
         }
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .pointerInput(Unit) {
-                awaitPointerEventScope {
-                    while (true) {
-                        // 1. 在 Initial 阶段观察按下，不消耗事件，确保 Pager 能收到
-                        val down =
-                            awaitFirstDown(
-                                requireUnconsumed = false,
-                                pass = PointerEventPass.Initial
-                            )
-                        // 2. 等待抬起
-                        val up = waitForUpOrCancellation(pass = PointerEventPass.Initial)
-                        // 3. 判定逻辑：只有在没被消费（说明不是滑动）且距离很短时触发
-                        if (up != null && !up.isConsumed) {
-                            val distance = (up.position - down.position).getDistance()
-                            if (distance < 10.dp.toPx()) {
-                                // --- 获取点击位置 ---
-                                val screenHeight = size.height
-                                val clickY = up.position.y
-
-                                when {
-                                    clickY < screenHeight / 3 -> {
-                                        comicReadViewModel.prev(context)
-                                        coroutineScope.launch {
-                                            lazyListState.scrollToItem(currentIndexState)
-                                        }
+    val currentConfig = LocalViewConfiguration.current
+    val customViewConfig = remember(currentConfig) {
+        object : ViewConfiguration by currentConfig {
+            // 双击检测改为 150 ms
+            override val doubleTapTimeoutMillis: Long
+                get() = 150L
+        }
+    }
+    val density = LocalDensity.current
+    CompositionLocalProvider(LocalViewConfiguration provides customViewConfig) {
+        val zoomState = rememberZoomableState(
+            zoomSpec = ZoomSpec(maxZoomFactor = 3f)
+        )
+        var size by remember { mutableStateOf(Size.Zero) }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .onSizeChanged {
+                    size = Size(it.width.toFloat(), it.height.toFloat())
+                }
+                .zoomable(
+                    state = zoomState,
+                    onClick = {
+                        val clickY = it.y
+                        when {
+                            clickY < size.height / 3 -> {
+                                coroutineScope.launch {
+                                    if (zoomState.contentTransformation.scale.scaleX != 1.0f) {
+                                        zoomState.resetZoom()
                                     }
-
-                                    clickY > screenHeight * 2 / 3 -> {
-                                        comicReadViewModel.next(context)
-                                        coroutineScope.launch {
-                                            lazyListState.scrollToItem(currentIndexState)
-                                        }
+                                    comicReadViewModel.hideToolBar()
+                                    lazyListState.scrollBy(-size.height)
+                                    if (lazyListState.firstVisibleItemIndex != currentIndexState) {
+                                        currentIndexState = lazyListState.firstVisibleItemIndex
+                                        comicReadViewModel.decodeIndex(currentIndexState, context)
                                     }
+                                }
+                            }
 
-                                    else -> {
-                                        comicReadViewModel.triggerToolBar()
+                            clickY > size.height * 2 / 3 -> {
+                                coroutineScope.launch {
+                                    if (zoomState.contentTransformation.scale.scaleX != 1.0f) {
+                                        zoomState.resetZoom()
                                     }
+                                    comicReadViewModel.hideToolBar()
+                                    lazyListState.scrollBy(size.height)
+                                    if (lazyListState.firstVisibleItemIndex != currentIndexState) {
+                                        currentIndexState = lazyListState.firstVisibleItemIndex
+                                        comicReadViewModel.decodeIndex(currentIndexState, context)
+                                    }
+                                }
+                            }
+
+                            else -> {
+                                coroutineScope.launch {
+                                    comicReadViewModel.triggerToolBar()
                                 }
                             }
                         }
                     }
-                }
-            }
-    ) {
-        LazyColumn(
-            state = lazyListState,
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            items(list, key = {
-                "${it.comicId}_${it.originSrc}"
-            }) {
-                ComicPicImage(
-                    comicPicImageState = it,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(
-                            when (val state = it.imageResultState) {
-                                is ImageResultState.Success -> {
-                                    state.decodeImageAspectRatio
-                                }
-
-                                else -> {
-                                    9f / 16
-                                }
-                            }
-                        )
                 )
+        ) {
+            LazyColumn(
+                state = lazyListState,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                items(list, key = {
+                    "${it.comicId}_${it.originSrc}"
+                }) {
+                    ComicPicImage(
+                        comicPicImageState = it,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(
+                                when (val state = it.imageResultState) {
+                                    is ImageResultState.Success -> {
+                                        state.decodeImageAspectRatio
+                                    }
+
+                                    else -> {
+                                        9f / 16
+                                    }
+                                }
+                            )
+                    )
+                }
             }
         }
     }
