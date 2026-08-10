eb3b6fe67b26ab6ca97a26b912519a2596cf0729
Dedicatus546 <qq1607611087@gmail.com>
2026-08-10 11:30:26 +0800

    feat: 本子详情页右上角 JM 号支持点击复制

--- app/src/main/java/com/par9uet/jm/ui/components/ComicCoverImage.kt
+++ app/src/main/java/com/par9uet/jm/ui/components/ComicCoverImage.kt
@@ -1,5 +1,6 @@
 package com.par9uet.jm.ui.components
 
+import android.content.ClipData
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,14 +12,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.platform.toClipEntry
 import androidx.compose.ui.unit.dp
 import coil.ImageLoader
 import coil.compose.AsyncImage
 import com.par9uet.jm.data.models.Comic
 import com.par9uet.jm.store.RemoteSettingManager
+import com.par9uet.jm.store.ToastManager
+import kotlinx.coroutines.launch
 import org.koin.compose.getKoin
 
 @Composable
@@ -26,8 +32,11 @@ fun ComicCoverImage(
     comic: Comic,
     showIdChip: Boolean = false,
     remoteSettingManager: RemoteSettingManager = getKoin().get(),
+    toastManager: ToastManager = getKoin().get(),
     imageLoader: ImageLoader = getKoin().get()
 ) {
+    val clipboard = LocalClipboard.current
+    val scope = rememberCoroutineScope()
     val remoteSetting by remoteSettingManager.remoteSettingState.collectAsState()
     Box(modifier = Modifier.fillMaxWidth()) {
         AsyncImage(
@@ -41,17 +50,23 @@ fun ComicCoverImage(
                 .fillMaxWidth(),
         )
         if (showIdChip) {
+            val text = "JM${comic.id}"
             AssistChip(
                 border = null,
                 colors = AssistChipDefaults.assistChipColors(
                     containerColor = MaterialTheme.colorScheme.surfaceContainer,
                 ),
-                modifier = Modifier.align(Alignment.TopEnd).padding(end = 10.dp, top = 10.dp),
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(end = 10.dp, top = 10.dp),
                 onClick = {
+                    scope.launch {
+                        val clipData = ClipData.newPlainText("本子号", text)
+                        clipboard.setClipEntry(clipData.toClipEntry())
 
                 },
                 label = {
-                    Text("JM${comic.id}")
+                    Text(text)
                 }
             )
         }
