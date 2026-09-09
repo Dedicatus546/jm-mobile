35def6720e2fdc1123971fcfcb7f97fc51486bdb
Dedicatus546 <qq1607611087@gmail.com>
2026-09-09 14:04:08 +0800

    feat: 完善关于页

--- app/src/main/java/com/par9uet/jm/ui/screens/AboutScreen.kt
+++ app/src/main/java/com/par9uet/jm/ui/screens/AboutScreen.kt
@@ -1,19 +1,20 @@
 package com.par9uet.jm.ui.screens
 
 import android.content.Intent
-import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowRight
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Alarm
 import androidx.compose.material.icons.filled.Code
+import androidx.compose.material.icons.filled.Commit
+import androidx.compose.material.icons.filled.Tag
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -26,11 +27,12 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
+import com.par9uet.jm.BuildConfig
 import com.par9uet.jm.R
 import com.par9uet.jm.ui.components.CommonScaffold
 import com.par9uet.jm.ui.components.SettingGroup
 import com.par9uet.jm.ui.components.SettingListItem
-import androidx.core.net.toUri
 
 @Composable
 fun AboutScreen() {
@@ -65,14 +67,34 @@ fun AboutScreen() {
                         fontSize = 28.sp,
                         fontWeight = FontWeight.Bold
                     )
-                    Text(
-                        text = "1.3.0(#5e28dce)",
-                        color = MaterialTheme.colorScheme.outline
-                    )
                 }
             }
             item {
                 SettingGroup {
+                    SettingListItem(
+                        icon = Icons.Default.Tag,
+                        iconContentDescription = "版本",
+                        title = "版本"
+                    ) {
+                        Text(BuildConfig.VERSION_NAME)
+                    }
+                    HorizontalDivider(color = MaterialTheme.colorScheme.background)
+                    SettingListItem(
+                        icon = Icons.Default.Commit,
+                        iconContentDescription = "Commit Hash",
+                        title = "Commit Hash"
+                    ) {
+                        Text("#${BuildConfig.COMMIT_HASH}")
+                    }
+                    HorizontalDivider(color = MaterialTheme.colorScheme.background)
+                    SettingListItem(
+                        icon = Icons.Default.Alarm,
+                        iconContentDescription = "构建时间",
+                        title = "构建时间"
+                    ) {
+                        Text(BuildConfig.BUILD_TIME)
+                    }
+                    HorizontalDivider(color = MaterialTheme.colorScheme.background)
                     SettingListItem(
                         icon = Icons.Default.Code,
                         iconContentDescription = "仓库",
